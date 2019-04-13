2b034f9e99a5ce746fa3c9e7f943aa3097221dd0
Brian Tobal <brian.tobal@gmail.com>
2019-04-13 18:35:18 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,25 +1,29 @@
 require 'pry'
 
 class Author 
-  attr_accessor :name 
+  attr_accessor :name #creates a name reader / writer 
   
   def initialize (name)
     @name = name 
-  end     #def 
+  end     #def  #you need to initialize the class w/ a name 
   
   def posts 
-    Post.all.select {|post| post.author=self} #we're getting all posts, and sorting by the author, which is why there's a self there 
+    Post.all.select {|post| post.author=self} 
+    #we're getting all posts the @@all method in Post 
+    #sorting by the author (.select) then iterating over the posts and grabbing author=self
+    #which is why there's a self there 
   end     #def 
   
   def add_post (new_post)
-    new_post.author = self 
+    new_post.author = self #this post we're passing belongs to the instance author
   end     #def 
+
+#  post_title  #A 
+# => "My Great Blog Post"
   
   def add_post_by_title (post_title)  
-    binding.pry
+    post_title.author = self    #needs to add a new post to betty 
+    Post.new(post_title)        #when you call betty.post.last.title you get add_post_by_title
+            #expect(betty.posts.last.author).to eq(betty)
   end     #def 
-
 end          #class 
-
-
-
