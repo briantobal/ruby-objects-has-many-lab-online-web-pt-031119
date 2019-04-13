de9489f48ff7db22555da298ebf5b51d85f7d9ee
Brian Tobal <brian.tobal@gmail.com>
2019-04-13 19:14:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,48 +1,51 @@
+Skip to content
+ 
+Search or jump to…
 
-class Artist 
-  attr_accessor :name 
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@briantobal 
+1
+0 27 briantobal/ruby-objects-has-many-lab-online-web-pt-031119
+forked from learn-co-students/ruby-objects-has-many-lab-online-web-pt-031119
+ Code  Pull requests 0  Projects 0  Insights  Settings
+ruby-objects-has-many-lab-online-web-pt-031119/lib/artist.rb
+@maxwellbenton maxwellbenton rebase from master, updated solution
+89d9c9b on Mar 7
+@MinesJA @maxwellbenton @SophieDeBenedetto @jmburges
+32 lines (24 sloc)  402 Bytes
+    
+class Artist
+  attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
-    @name = name 
-    @songs = [ ]
-    end     #def 
+    @name = name
+    @@all << self
+  end
 
-  def songs 
-    @songs 
-    end     #def 
-    
+  def self.all
+    @@all
+  end
+
   def add_song(song)
-    @songs << song 
-    song.artist = self 
-    end     #def 
+    song.artist = self
+  end
 
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    song.artist = self 
-    @songs << song
-    end     #def 
-    
-  def self.song_count 
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
     Song.all.count
-  end     #def 
-
-end #class 
-
-#---- 
-
-class Song 
-  attr_accessor :artist, :name
-  @@all = [ ]
-
-  def initialize (name)
-    @name = name 
-    @@all << self 
-  end     #def 
-  
-  def self.all 
-    @@all 
-  end     #def 
-  
-
-end #class 
-
+  end
+end
